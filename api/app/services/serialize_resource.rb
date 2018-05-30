1d39a5e0f086358dcc163ccfec4e6c7fbb591656
regularmike <regularmike@gmail.com>
2018-05-30 02:49:25 +0000

    add basic 'instance of' statement

--- api/app/services/serialize_resource.rb
+++ api/app/services/serialize_resource.rb
@@ -1,6 +1,18 @@
 class SerializeResource
   def self.call(person)
-    graph = RDF::Graph.new << [:hello, RDF::RDFS.label, "Hello, world!"]
-    graph.to_ttl
+    graph = RDF::Graph.new << [
+			RDF::URI("http://purl.bdrc.io/resource/#{person.tbrc_rid}"), 
+		  RDF.type, 
+			RDF::URI("http://purl.bdrc.io/ontology/core/Person")
+		]
+
+		RDF::Turtle::Writer.buffer(prefixes: {
+			bdr: "http://purl.bdrc.io/resource/",
+		  bdo: "http://purl.bdrc.io/ontology/core/"
+		}) do |writer|
+  		graph.each_statement do |statement|
+    		writer << statement
+		  end
+		end
   end
 end
