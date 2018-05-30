4dfb838174818a5d891ffcd9bee677c7185211e8
regularmike <regularmike@gmail.com>
2018-05-30 03:19:48 +0000

    fix formatting

--- api/app/services/serialize_resource.rb
+++ api/app/services/serialize_resource.rb
@@ -1,18 +1,21 @@
+# static methods for serializing Active Record resources
 class SerializeResource
   def self.call(person)
     graph = RDF::Graph.new << [
-			RDF::URI("http://purl.bdrc.io/resource/#{person.tbrc_rid}"), 
-		  RDF.type, 
-			RDF::URI("http://purl.bdrc.io/ontology/core/Person")
-		]
+      RDF::URI("http://purl.bdrc.io/resource/#{person.tbrc_rid}"),
+      RDF.type,
+      RDF::URI("http://purl.bdrc.io/ontology/core/Person")
+    ]
 
-		RDF::Turtle::Writer.buffer(prefixes: {
-			bdr: "http://purl.bdrc.io/resource/",
-		  bdo: "http://purl.bdrc.io/ontology/core/"
-		}) do |writer|
-  		graph.each_statement do |statement|
-    		writer << statement
-		  end
-		end
+    RDF::Turtle::Writer.buffer(
+      prefixes: {
+        bdr: "http://purl.bdrc.io/resource/",
+        bdo: "http://purl.bdrc.io/ontology/core/"
+      }
+    ) do |writer|
+      graph.each_statement do |statement|
+        writer << statement
+      end
+    end
   end
 end
