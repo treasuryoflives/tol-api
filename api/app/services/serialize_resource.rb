967c1797b42ee4744c9eda993c3f60c1627822e3
regularmike <regularmike@gmail.com>
2018-07-04 17:41:24 +0000

    add name triples

--- api/app/services/serialize_resource.rb
+++ api/app/services/serialize_resource.rb
@@ -4,7 +4,8 @@ class SerializeResource
     tol: "http://api.treasuryoflives.org/resource/",  
     bdr: "http://purl.bdrc.io/resource/",
     bdo: "http://purl.bdrc.io/ontology/core/",
-    owl: "http://www.w3.org/2002/07/owl#"
+    owl: "http://www.w3.org/2002/07/owl#",
+    rdfs: "http://www.w3.org/2000/01/rdf-schema#"
   }
 
   def initialize(person)
@@ -31,13 +32,15 @@ class SerializeResource
   def add_triples
     add_type
     add_bdr_link
+    add_default_name
+    add_default_title
   end
 
   def add_type
     @graph << [
       RDF::URI(create_resource(:tol, "P#{@person.person_id}")),
       RDF.type,
-      RDF::URI(create_resource(:bdo, "Person"))
+      RDF::URI(create_resource(:bdo, 'Person'))
     ]
   end
 
@@ -46,6 +49,22 @@ class SerializeResource
       RDF::URI(create_resource(:tol, "P#{@person.person_id}")),
       RDF::OWL.sameAs,
       RDF::URI(create_resource(:bdr, @person.tbrc_rid))
+    ]
+  end
+
+  def add_default_name
+    @graph << [
+      RDF::URI(create_resource(:bdo, 'PersonPrimaryName')),
+      RDF::RDFS.label,
+      @person.default_name
+    ]
+  end
+
+  def add_default_title
+    @graph << [
+      RDF::URI(create_resource(:bdo, 'PersonPrimaryTitle')),
+      RDF::RDFS.label,
+      @person.default_title
     ]
   end
 end
