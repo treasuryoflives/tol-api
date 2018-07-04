52fe6c697faf4f182a7c9a4910edfa3dbb680dc5
regularmike <regularmike@gmail.com>
2018-07-04 15:18:07 +0000

    switch to tol identifiers

--- api/app/controllers/resource_controller.rb
+++ api/app/controllers/resource_controller.rb
@@ -1,8 +1,11 @@
 class ResourceController < ApplicationController
 	def show
-		person_id = params[:id]
-		@person = Person.find_by_tbrc_rid(person_id)
-		@person_json = JSON.pretty_generate(@person.as_json)
-    @person_rdf = SerializeResource.call(@person) 
+		id = params[:id]
+    case id
+    when /^P/
+      @person = Person.find_by_person_id(id[1..-1])
+      @person_json = JSON.pretty_generate(@person.as_json)
+      @person_rdf = SerializeResource.new(@person).serialize 
+    end
 	end	
 end
--- api/app/services/serialize_resource.rb
+++ api/app/services/serialize_resource.rb
@@ -1,21 +1,51 @@
-# static methods for serializing Active Record resources
+# serializes an Active Record resources as RDF
 class SerializeResource
-  def self.call(person)
-    graph = RDF::Graph.new << [
-      RDF::URI("http://purl.bdrc.io/resource/#{person.tbrc_rid}"),
-      RDF.type,
-      RDF::URI("http://purl.bdrc.io/ontology/core/Person")
-    ]
+  PREFIXES = {
+    tol: "http://api.treasuryoflives.org/resource/",  
+    bdr: "http://purl.bdrc.io/resource/",
+    bdo: "http://purl.bdrc.io/ontology/core/",
+    owl: "http://www.w3.org/2002/07/owl#"
+  }
 
-    RDF::Turtle::Writer.buffer(
-      prefixes: {
-        bdr: "http://purl.bdrc.io/resource/",
-        bdo: "http://purl.bdrc.io/ontology/core/"
-      }
-    ) do |writer|
-      graph.each_statement do |statement|
+  def initialize(person)
+    @person = person
+    @graph = RDF::Graph.new
+  end
+
+  def serialize
+    add_triples
+
+    RDF::Turtle::Writer.buffer(prefixes: PREFIXES) do |writer|
+      @graph.each_statement do |statement|
         writer << statement
       end
     end
+  end
+
+  private
+
+  def create_resource(prefix, name)
+    PREFIXES[prefix] + name
+  end
+
+  def add_triples
+    add_type
+    add_bdr_link
+  end
+
+  def add_type
+    @graph << [
+      RDF::URI(create_resource(:tol, "P#{@person.person_id}")),
+      RDF.type,
+      RDF::URI(create_resource(:bdo, "Person"))
+    ]
+  end
+
+  def add_bdr_link
+    @graph << [
+      RDF::URI(create_resource(:tol, "P#{@person.person_id}")),
+      RDF::OWL.sameAs,
+      RDF::URI(create_resource(:bdr, @person.tbrc_rid))
+    ]
   end
 end
