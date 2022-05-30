b20e0d0f03445f1afe48470fcd4496cb4ee1cdcc
Michael Sullivan <regularmike@gmail.com>
2022-05-30 11:55:45 -0400

    add bdrc handling

--- api/app/controllers/resource_controller.rb
+++ api/app/controllers/resource_controller.rb
@@ -1,37 +1,42 @@
 class ResourceController < ApplicationController
   
 	def show
-
 		id = params[:id]
+
     case id
     when /^TOLP/
       @person = Person
-                .eager_load(name_variants: :encoding_type)
-                .find_by_person_id(id[4..-1]) or not_found
+        .eager_load(name_variants: :encoding_type)
+        .find_by_person_id(id[4..-1]) or not_found
+    when /^BDRCP/
+        return render plain: BdrcResourceToJson.new(id[4..-1]).serialize 
     else
-      not_found
+        not_found
     end
 
-respond_to do |format| 
+    respond_to do |format| 
       format.html do
-        redirect_to @person.tol_link        
+          redirect_to @person.tol_link        
       end
+      
       format.jsonld do
-        render_json
+          render_json
       end
+      
       format.json do
-        render_json
+          render_json
       end
 
       format.ttl do
-        render plain: SerializeResource.new(@person, :ttl).serialize 
+          render plain: SerializeResource.new(@person, :ttl).serialize 
       end
 
       format.rdf do
-        render_rdfxml
+          render_rdfxml
       end
+      
       format.xml do
-        render_rdfxml
+          render_rdfxml
       end
     end
 	end	
---
+++ api/app/services/bdrc_resource_to_json.rb
@@ -0,0 +1,12 @@
+# serializes an Active Record resource as RDF
+class BdrcResourceToJson
+  def initialize(bdrc_id)
+    
+  end  
+
+  def serialize
+    return 'test'
+  end
+
+  private  
+end
